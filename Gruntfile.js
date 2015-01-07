10e209dfaaf1546ecea02e76582c040720e5b6af
Ash Durham <ash@matterdesign.com.au>
2015-01-07 14:53:53 +1100

    Adding grunt functionality

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        copy: {
+            wp: {
+                expand: true,
+                cwd: 'wp/',
+                src: ['*'],
+                dest: './'
+//                options: {
+//                    process: function(content, srcpath) {
+//                        return content.replace(/wp\-/g, "wp/wp-");
+//                    }
+//                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('default', ['copy:wp', 'update_wp_paths']);
+
+    grunt.registerTask('update_wp_paths', function() {
+
+        var fs = require('fs');
+        var path = require('path');
+        
+        grunt.file.delete('.git/');
+        grunt.file.delete('wp/');
+        grunt.file.delete('composer.json');
+        grunt.file.delete('package.json');
+        grunt.file.delete('readme.md');
+        grunt.file.delete('Gruntfile.js');
+    });
+}
